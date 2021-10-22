5d9ad7aeb3d4554a4cf93783e0340fbb96557a2d
kolop <kolop@SHANDOR>
2021-10-22 09:13:57 +0300

    Добавьте файлы проекта.

---
+++ Project_12_1_4/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Project_12_1_4;
+
+Console.WriteLine("Введите ваш логин");
+
+User user = new User();
+
+user.Login = Console.ReadLine();
+
+Console.WriteLine("Добро пожаловать! {0}", user.Name());
+
+if (user.IsPremium() == true)
+{
+
+}
+else
+{
+	Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+	Thread.Sleep(1000);
+
+	Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+	Thread.Sleep(2000);
+
+	Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+	Thread.Sleep(3000);
+}
+
+Console.ReadKey();
+
---
+++ Project_12_1_4/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_12_1_4
+{
+    internal class User
+    {
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Имитация взаимодействия с API, получение данных по ключевому слову Login (Типо работа с бд через API)
+        /// </summary>
+        /// <returns></returns>
+        public string Name()
+        {
+            return "MyName";
+        }
+
+
+        /// <summary>
+        /// Имитация взаимодействия с API, получение данных по ключевому слову Login (Типо работа с бд через API)
+        /// </summary>
+        /// <returns></returns>
+        public bool IsPremium()
+        {
+            return false;
+        }
+    }
+}
